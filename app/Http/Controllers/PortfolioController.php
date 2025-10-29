e6bb722540b622d24d5c60c2297a167e64845fd2
npvargas <npvargas@dswd.gov.ph>
2025-10-29 11:08:07 +0800

    add profile picture

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -23,10 +23,9 @@ class PortfolioController extends Controller
             'email' => env('PORTFOLIO_EMAIL', 'hello@example.com'),
             'phone' => env('PORTFOLIO_PHONE', '+1 (555) 123-4567'),
             'location' => env('PORTFOLIO_LOCATION', 'San Francisco, CA'),
-            'profile_image' => env('PORTFOLIO_PROFILE_IMAGE', 'https://ui-avatars.com/api/?name=John+Doe&size=400'),
+            'profile_image' =>env('APP_URL') . env('PORTFOLIO_PROFILE_IMAGE', 'https://ui-avatars.com/api/?name=Nelson+Vargas&size=400'),
             'resume_url' => env('PORTFOLIO_RESUME_URL', '#'),
         ];
-
         // Parse social links from env
         $socialLinks = collect([
             env('PORTFOLIO_SOCIAL_GITHUB'),
