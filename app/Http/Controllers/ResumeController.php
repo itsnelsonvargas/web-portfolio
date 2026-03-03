7db006d12686ac874505c4ce4980194da1acf947
Nelson Vargas <itsnelsonvargas@gmail.com>
2026-03-03 16:54:05 +0800

    fix: change the redirect link for Dev resume to the correct link.

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -12,7 +12,7 @@ class ResumeController extends Controller
         return match ($category) {
             'pm' => redirect()->away('https://www.canva.com/design/DAHCnxLewS4/EyKnMi8U7nZcZzGVDWTHww/edit?utm_content=DAHCnxLewS4&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'),
 
-            'dev' => redirect()->away('https://drive.google.com/drive/folders/1Q37Vv1HtMCmLBKvg0XG6WA1B7uvZQziW'),
+            'dev' => redirect()->away('https://www.canva.com/design/DAGtyKG958Y/KpGNkYhAltA6NPCv0UywLA/edit?utm_content=DAGtyKG958Y&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'),
 
             default => abort(404),
         };
