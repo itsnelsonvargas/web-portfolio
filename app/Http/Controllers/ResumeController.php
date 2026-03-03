a1da4031a60dc27aac34690a3ca3e92dc33df53b
Nelson Vargas <itsnelsonvargas@gmail.com>
2026-03-03 16:49:28 +0800

    refactor: restructure code to prepare the index( ) function to add more resume in the future.

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -3,20 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class ResumeController extends Controller
 {
-    public function index($category)
+    public function index(string $category): RedirectResponse
     {
-        if( $category === 'pm') {
-            // For project manager resume
-            return redirect('https://www.canva.com/design/DAHCnxLewS4/EyKnMi8U7nZcZzGVDWTHww/edit?utm_content=DAHCnxLewS4&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton');
-       
-        }else if( $category === 'dev') {
-            // For developer resume
-            return redirect('https://drive.google.com/drive/folders/1Q37Vv1HtMCmLBKvg0XG6WA1B7uvZQziW');
-        }
+        return match ($category) {
+            'pm' => redirect()->away('https://www.canva.com/design/DAHCnxLewS4/EyKnMi8U7nZcZzGVDWTHww/edit?utm_content=DAHCnxLewS4&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'),
 
-        return "";
+            'dev' => redirect()->away('https://drive.google.com/drive/folders/1Q37Vv1HtMCmLBKvg0XG6WA1B7uvZQziW'),
+
+            default => abort(404),
+        };
     }
 }
