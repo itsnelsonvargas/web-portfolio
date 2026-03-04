ede5aad807c9815562ff911e8e30c37a0b0fdf29
Nelson Vargas <itsnelsonvargas@gmail.com>
2026-03-04 10:03:45 +0800

    feat: add a redirect link for Pentest resume

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -14,6 +14,8 @@ class ResumeController extends Controller
 
             'dev' => redirect()->away('https://www.canva.com/design/DAGtyKG958Y/KpGNkYhAltA6NPCv0UywLA/edit?utm_content=DAGtyKG958Y&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'),
 
+            'pentest' => redirect()->away('https://www.canva.com/design/DAG7UJty_B8/TSjdKkxvc84hcD5G3c_JCQ/edit?utm_content=DAG7UJty_B8&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton');
+
             default => abort(404),
         };
     }
