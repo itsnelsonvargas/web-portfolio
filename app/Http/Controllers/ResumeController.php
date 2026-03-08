dcb4f9917972d5155471830c03342afc5441a900
nelson <itsnelsonvargas@gmail.com>
2026-03-08 23:35:41 +0800

    docs: add a TODO in the resumeController

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -10,6 +10,10 @@ class ResumeController extends Controller
     public function index(string $category): RedirectResponse
     {
         return match ($category) {
+
+            //TODO: Convert canva to PDF.
+
+
             //Project Management
             'pm' => redirect()->away('https://www.canva.com/design/DAHCnxLewS4/EyKnMi8U7nZcZzGVDWTHww/edit?utm_content=DAHCnxLewS4&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'),
             
