8fbaa7ecbfae2551d1355c8b994d875b1a6a785a
nelson <itsnelsonvargas@gmail.com>
2026-03-21 18:08:28 +0800

    content: change the latest TOR template

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -8,8 +8,8 @@ class DownloadController extends Controller
 {
     public function downloadTor(Request $request)
     {
-        $torLink = "https://docs.google.com/document/d/1BBKtoUTa1Vygrq00tDYHxfc2IP6PxIcs/edit";
-
+       // $torLink = "https://docs.google.com/document/d/1BBKtoUTa1Vygrq00tDYHxfc2IP6PxIcs/edit";
+        $torLink = "https://docs.google.com/document/d/1bcMiEllkXxYuDUiY0N0Q2Nt_pyTWz-fmIKRo0kjQCFI/edit";
         // Convert the Google Doc link to a "copy" link
         if (str_contains($torLink, 'docs.google.com/document/d/')) {
             $torLink = preg_replace('/\/edit.*$/', '/copy', $torLink);
