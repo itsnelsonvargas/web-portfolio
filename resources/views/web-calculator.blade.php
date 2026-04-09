1704a4e10e5b1f8552162e29037d459493838701
nelson <itsnelsonvargas@gmail.com>
2026-04-09 10:00:00 +0800

    feat: create a view for web cost calculator

---
+++ resources/views/web-calculator.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Project Calculator</title>
+</head>
+<body>
+    <main>
+        <h1>Web Project Calculator</h1>
+        <p>This page will generate an estimated amount for a project.</p>
+    </main>
+</body>
+</html>
