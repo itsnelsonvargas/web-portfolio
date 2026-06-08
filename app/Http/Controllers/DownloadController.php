80e534b1575e7d3bef808a91e92e138398f1f0ed
nelson <itsnelsonvargas@gmail.com>
2026-06-08 20:39:55 +0800

    feat: log download requests in DownloadController for tracking user activity

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class DownloadController extends Controller
 {
     public function downloadTor(Request $request)
     {
+        Log::info('Tor download visited', [
+            'ip' => $request->ip(),
+            'user_agent' => $request->userAgent(),
+            'referer' => $request->headers->get('referer'),
+        ]);
+
        // $torLink = "https://docs.google.com/document/d/1BBKtoUTa1Vygrq00tDYHxfc2IP6PxIcs/edit";
         $torLink = "https://docs.google.com/document/d/1bcMiEllkXxYuDUiY0N0Q2Nt_pyTWz-fmIKRo0kjQCFI/edit";
         // Convert the Google Doc link to a "copy" link
